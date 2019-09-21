d0a3b84b423c4b6a2b4bb7422b0080970fe92e87
slizbethellis <slizbethellis@gmail.com>
2019-09-21 12:42:51 -0500

    bulma forms added

--- purgify/src/App.js
+++ purgify/src/App.js
@@ -1,63 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
-<<<<<<< HEAD
 // import './App.css';
+import Heading from 'react-bulma-components/lib/components/heading';
+import Box from 'react-bulma-components/lib/components/box';
 import Columns from 'react-bulma-components/lib/components/columns';
-import {
-  Field,
-  Control,
-  Label,
-  Input,
-  Textarea,
-  Select,
-  Checkbox,
-  Radio,
-  Help,
-} from 'react-bulma-components/lib/components/form';
+import { Field, Control, Label, Input } from 'react-bulma-components/lib/components/form';
 import Button from 'react-bulma-components/lib/components/button';
-=======
-import Searc from './components/Search';
-import './App.css';
->>>>>>> e038add6b60a7aa7e79e813b7af5ca1cdc1bc4b7
+import Section from 'react-bulma-components/lib/components/section';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Purgify Spotify</h2> 
-        <p>
-        It's time to purge your Spotify of the music you don't listen to.
-        </p>
-      </header>
-      <Columns>
-        <Columns.Column size={6} offset={3}>
-          <div className="search">
-            <Field>
-              <Label>Playlists</Label>
-              <Control>
-                <Input placeholder="Playlist name" />
-              </Control>
-              <Control>
-                <Button type="primary">Search Playlists</Button>
-              </Control>
-            </Field>
-          </div>
-        </Columns.Column>
-      </Columns>
-      <div className="search">
-
-        <p> Pick a Playlist </p>
-        <input type="text" placeholder="Search Playlist" />
-
-        <p> Pick an artist </p>
-        <input type="text" placeholder="Search Artist" />
-
-        <p> Pick a song </p>
-        <input type="text" placeholder="Search Songs" />
-
-        
-      </div>
-      <button className="purgeButton" type="submit"> Purge </button>
+      <Box>
+        <Heading className="has-text-centered">Purgify Spotify</Heading>
+        <Heading subtitle size={6} className="has-text-centered">
+          It's time to purge your Spotify of the music you don't listen to.
+        </Heading>
+      </Box>
+      <Section>
+        <Columns>
+          <Columns.Column size={6} offset={3}>
+            <div className="search">
+              <Field>
+                <Label>Playlist</Label>
+                <Control>
+                  <Input placeholder="Playlist name" />
+                </Control>
+                <Control>
+                  <Button type="primary">Search Playlists</Button>
+                </Control>
+              </Field>
+              <Field>
+                <Label>Artist</Label>
+                <Control>
+                  <Input placeholder="Artist name" />
+                </Control>
+                <Control>
+                  <Button type="primary">Search Artists</Button>
+                </Control>
+              </Field>
+              <Field>
+                <Label>Song</Label>
+                <Control>
+                  <Input placeholder="Song name" />
+                </Control>
+                <Control>
+                  <Button type="primary">Search Songs</Button>
+                </Control>
+              </Field>
+            </div>
+          </Columns.Column>
+        </Columns>
+      </Section>
     </div>
   );
 }
